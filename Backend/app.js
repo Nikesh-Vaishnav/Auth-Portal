cc9fb3e3b8f089ec47d99b40421c0caf76bb5573
Nikesh-Vaishanve <nikeshvaishnav377@gmmail.com>
2025-06-13 12:32:05 +0530

    improve backend

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,6 @@
+const dotenv = require('dotenv');
 const express = require('express');
 const cors = require('cors');
-const dotenv = require('dotenv');
 const path = require('path');
 
 // Load environment variables
